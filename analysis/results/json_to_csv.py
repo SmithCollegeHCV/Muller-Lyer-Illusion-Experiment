28d5e208951350af20bc4588b74d453dcffc0b1f
smithieincs <xtan@smith.edu>
2018-06-13 12:30:45 -0400

    Added platform.js

--- analysis/results/json_to_csv.py
+++ analysis/results/json_to_csv.py
@@ -11,9 +11,9 @@ dataFrame = pd.DataFrame(data = data)
 #DataFrame with time features
 dataFrame_withtime = dataFrame.loc[:,["postId","workerId","pageId","conditionVersion","s1","s2","s3","s4","s5","s6","age","gender","degree","monitor","comments",
                                      "time_start_experiment","time_end_experiment","time_start_tutorial","time_end_tutorial","time_start_test",
-                                     "time_end_test","time_start_demographics","time_end_demographics"]]
+                                     "time_end_test","time_start_demographics","time_end_demographics","platform"]]
 dataFrame_withtime.to_csv("data_with_time.csv", index = False)
 
 #DataFrame without time features
-dataFrame_notime = dataFrame.loc[:,["postId","workerId","pageId","conditionVersion","s1","s2","s3","s4","s5","s6","age","gender","degree","monitor","comments"]]
+dataFrame_notime = dataFrame.loc[:,["postId","workerId","pageId","conditionVersion","s1","s2","s3","s4","s5","s6","age","gender","degree","monitor","comments","platform"]]
 dataFrame_notime.to_csv("data_no_time.csv", index = False)
